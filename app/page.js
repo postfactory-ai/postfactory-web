22695b51c1a6806327344054065add1130715c8f
postfactory-ai <krstygbsn@gmail.com>
2025-12-26 15:24:27 +0300

    Fix import paths in page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Hero from '@/components/Hero'
-import Features from '@/components/Features'
-import HowItWorks from '@/components/HowItWorks'
-import Pricing from '@/components/Pricing'
-import Testimonials from '@/components/Testimonials'
-import CTA from '@/components/CTA'
+import Hero from './../components/Hero'
+import Features from './../components/Features'
+import HowItWorks from './../components/HowItWorks'
+import Pricing from './../components/Pricing'
+import Testimonials from './../components/Testimonials'
+import CTA from './../components/CTA'
 
 export default function Home() {
   return (
