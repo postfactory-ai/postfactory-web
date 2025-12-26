eaee843eb53f0b38e181442290b1aed2dae40081
postfactory-ai <krstygbsn@gmail.com>
2025-12-26 15:06:29 +0300

    Add home page

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import HowItWorks from '@/components/HowItWorks'
+import Pricing from '@/components/Pricing'
+import Testimonials from '@/components/Testimonials'
+import CTA from '@/components/CTA'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Testimonials />
+      <Pricing />
+      <CTA />
+    </>
+  )
+}
