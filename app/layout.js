1bf9ed90dc412411ff33a82a715374aadfc278b1
postfactory-ai <krstygbsn@gmail.com>
2025-12-26 18:46:42 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from './../components/Navbar'
 import Footer from './../components/Footer'
 import { LanguageProvider } from './../contexts/LanguageContext'
 
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <LanguageProvider>
-          <Navbar />
           <main className="min-h-screen">
             {children}
           </main>
